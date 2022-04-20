d82176bb046210cdd8183781a291498446d45c08
Dimpho <DimphoR@bayportfinance.com>
2022-04-20 14:39:15 +0200

    change service column to service type

--- Diy-Project/Scripts/createServiceProvidersScript.sql
+++ Diy-Project/Scripts/createServiceProvidersScript.sql
@@ -5,7 +5,7 @@ CREATE TABLE [dbo].[Services]
 (
     [ID] [INT] PRIMARY KEY IDENTITY(1,1),                        -- primary key column
     [ProviderName] [VARCHAR](50) NOT NULL,
-    [Service] [VARCHAR](50) NOT NULL,
+    [ServiceType] [VARCHAR](50) NOT NULL,
     [Description] [VARCHAR](250) NOT NULL,            
     [CallOutFee] [SmallMoney] NOT NULL,
     [UserID] [INT] NOT NULL
