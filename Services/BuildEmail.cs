fc5c6377f507bb9a1470b91b7470224659d5f03b
IKalonji2 <Issa@bbd.co.za>
2022-04-20 21:41:50 +0200

    Added constant variable to From method in BuildEmail

--- Services/BuildEmail.cs
+++ Services/BuildEmail.cs
@@ -2,28 +2,28 @@
 
 namespace Diy_Project.Services
 {
-    public class EmailMessageFluentApi
+    public class BuildEmail
     {
         private MimeMessage _message;
 
-        public EmailMessageFluentApi()
+        public BuildEmail()
         {
             _message = new MimeMessage();
         }
 
-        public EmailMessageFluentApi From(string name, string mailbox)
+        public BuildEmail From(string name = "DIY Project", string mailbox = "diyprojectcsharp@gmail.com")
         {
             _message.From.Add(new MailboxAddress(name, mailbox));
             return this;
         }
 
-        public EmailMessageFluentApi To(string address)
+        public BuildEmail To(string address)
         {
             _message.To.Add(MailboxAddress.Parse(address));
             return this;
         }
 
-        public EmailMessageFluentApi MailContents(EmailType emailType)
+        public BuildEmail MailContents(EmailType emailType)
         {
             _message.Subject = EmailMessages.EmailSubject(emailType);
             _message.Body = EmailMessages.EmailBody(emailType);
